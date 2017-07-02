ee80f1147d7f2d52b77d2139090c14bd5fd21f1c
Tastes Like Turkey <esbe118@live.com>
2017-07-01 20:47:17 -0400

    Add EBook model.

---
+++ Papyrus/Papyrus/Models/Ebook.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Storage;
+
+namespace Papyrus.Models
+{
+    public class EBook : BaseNotify
+    {
+        public EBook() { }
+
+        public EBook(StorageFolder folder)
+        {
+
+        }
+
+        #region Title
+
+        private string _title = default(string);
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        #endregion Title
+    }
+}
