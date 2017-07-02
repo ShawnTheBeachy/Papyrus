3bd3e41661eea6e3649dca30bb7a6e3dadeafb89
Tastes Like Turkey <esbe118@live.com>
2017-07-01 20:55:54 -0400

    Add extension to verify mimetype.

--- Papyrus/Papyrus/Extensions/EbookExtensions.cs
+++ Papyrus/Papyrus/Extensions/EbookExtensions.cs
@@ -1,7 +1,24 @@
-﻿namespace Papyrus
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Papyrus
 {
     public static class EBookExtensions
     {
+        public static async Task<bool> VerifyMimetypeAsync(this EBook ebook)
+        {
+            var mimetypeFile = await ebook._rootFolder.GetItemAsync("mimetype");
 
+            if (mimetypeFile == null)                                           // Make sure file exists.
+                return false;
+
+            var fileContents = await FileIO.ReadTextAsync(mimetypeFile as StorageFile);
+
+            if (fileContents != "application/epub+zip")                         // Make sure file contents are correct.
+                return false;
+
+            return true;
+        }
     }
 }
