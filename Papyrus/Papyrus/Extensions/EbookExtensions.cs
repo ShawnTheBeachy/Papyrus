2274bf1f10b70248ab249ffd92209a69a4952961
Tastes Like Turkey <esbe118@live.com>
2017-07-01 21:17:06 -0400

    Use local function to verify mimetype string.

--- Papyrus/Papyrus/Extensions/EbookExtensions.cs
+++ Papyrus/Papyrus/Extensions/EbookExtensions.cs
@@ -8,6 +8,12 @@ namespace Papyrus
     {
         public static async Task<bool> VerifyMimetypeAsync(this EBook ebook)
         {
+            bool VerifyMimetypeString(string value) =>
+                value == "application/epub+zip";
+
+            if (ebook._rootFolder == null)                                      // Make sure a root folder was specified.
+                return false;
+
             var mimetypeFile = await ebook._rootFolder.GetItemAsync("mimetype");
 
             if (mimetypeFile == null)                                           // Make sure file exists.
@@ -15,7 +21,7 @@ namespace Papyrus
 
             var fileContents = await FileIO.ReadTextAsync(mimetypeFile as StorageFile);
 
-            if (fileContents != "application/epub+zip")                         // Make sure file contents are correct.
+            if (!VerifyMimetypeString(fileContents))                         // Make sure file contents are correct.
                 return false;
 
             return true;
