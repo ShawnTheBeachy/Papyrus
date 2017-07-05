4cfdfecb8fcb6b511acbbc5b9e59a4c591ecab29
Tastes Like Turkey <esbe118@live.com>
2017-07-05 16:54:52 -0400

    Create new project and start with Toc control.

---
+++ Papyrus/Papyrus.UI/TableOfContents.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Papyrus.UI
+{
+    public sealed partial class TableOfContents : UserControl
+    {
+        #region Dependency properties
+
+        #region Source
+
+        public Papyrus.TableOfContents Source
+        {
+            get { return (Papyrus.TableOfContents)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+        
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(Papyrus.TableOfContents), typeof(TableOfContents), new PropertyMetadata(null));
+
+        #endregion Source
+
+        #endregion Dependency properties
+
+        public TableOfContents()
+        {
+            InitializeComponent();
+        }
+    }
+}
